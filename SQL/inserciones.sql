b80758a5fcdca407f8e17c8c19be1a1a05f6eda1
CAMI <camicalcagno12@gmail.com>
2015-05-15 09:17:33 -0300

    todo

---
+++ SQL/inserciones.sql
@@ -0,0 +1,14 @@
+INSERT INTO `BaseDeDatos`.`Usuario`(`dni`,`nombre`,`apellido`)
+VALUES
+("20200789","Jose","Ortiz"),
+("20340398","Martin","Ocampo"),
+("28374099","Laura","Bustos"),
+("30765111","Francisco","Martinez"),
+("33456789","Juan","Perez"),
+("36300981","Josefina","Campero"),
+("36453356","Agustin","Martinez"),
+("36453763","Santiago","Passalacqua"),
+("37255998","Virginia","Lopez"),
+("38339428","Camila","Calcagno"),
+("40988772","Mateo","Calcagno");
+
