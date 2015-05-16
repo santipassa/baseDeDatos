a328b9b1440c6d70fb14b95b374b0ccffa70a433
Santiago Passalacqua <passalacquasantiago@gmail.com>
2015-05-15 23:38:41 -0300

    trigger control xy

--- SQL/trigger_control_xy.sql
+++ SQL/trigger_control_xy.sql
@@ -3,6 +3,10 @@ CREATE TRIGGER trigger_control_xy
 BEFORE INSERT ON Celda
 FOR EACH ROW
 BEGIN
-	--aca hay que poner el codigo
+	CREATE VIEW join_Celda_Tiene AS SELECT Tiene.id_celda,fila,columna,codigo_partida,orden FROM Celda,Tiene where NEW.id_celda=Tiene.id_celda;
+	CREATE VIEW join_partida AS SELECT * FROM Partida NATURAL JOIN join_Celda_Tiene;
+	IF NEW.fila>=0 and NEW.fila<=join_partida.dimension_y_tablero AND 
+		NEW.columna>=0 and NEW.columna<=join_partida.dimension_x_tablero THEN
+		INSERT INTO Celda values(new.id_celda,new.fila,new.columna);
 END;
 $$ delimiter ;
