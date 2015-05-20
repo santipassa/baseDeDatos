09764e354c1daddcbada2424e379410c1f46ea6c
CAMI <camicalcagno12@gmail.com>
2015-05-19 21:46:59 -0300

    script

--- SQL/proyectobase.sql
+++ SQL/proyectobase.sql
@@ -21,25 +21,26 @@ fecha_hora_inicio datetime not null,
 fecha_hora_fin datetime not null,
 dni_jugador_1 varchar(8) not null, 
 dni_jugador_2 varchar(8) not null, 
-dni_ganador varchar(8) ,
+ganador varchar(8),
 constraint fk_jugador_1 foreign key (dni_jugador_1) references Usuario(dni) on delete cascade on update cascade, 
-constraint fk_jugador_2 foreign key (dni_jugador_2) references Usuario(dni) on delete cascade on update cascade,
-constraint fk_jugador_ganador foreign key (dni_ganador) references Usuario(dni) on delete cascade on update cascade) ;
+constraint fk_jugador_2 foreign key (dni_jugador_2) references Usuario(dni) on delete cascade on update cascade);
 
 --TABLA DE CELDAS
 CREATE TABLE Celda 
-(id_celda integer not null primary key auto_increment,
-fila integer not null,
-columna integer not null);
+(fila integer not null,
+columna integer not null,
+constraint pri primary key(fila,columna));
 
 --TABLA DE RELACION TIENE
+drop table if exists Tiene;
 CREATE TABLE Tiene 
 (codigo_partida integer not null, 
-id_celda integer not null, 
 orden integer not null, 
-constraint pk primary key (codigo_partida,id_celda),
-constraint fk_partida foreign key (codigo_partida) references Partida(codigo_partida) on delete cascade on update cascade,
-constraint fk_celda foreign key (id_celda) references Celda(id_celda) on delete cascade on update cascade );
+fila integer not null,
+columna integer not null,
+constraint pp primary key (codigo_partida,fila,columna),
+constraint fk_par foreign key (codigo_partida) references Partida(codigo_partida) on delete cascade on update cascade,
+constraint fk_fil foreign key (fila) references Celda(fila) on delete cascade on update cascade);
 
 --TABLA DE AUDITORIA DE LA BASE DE DATOS
 CREATE TABLE baja_usuarios
