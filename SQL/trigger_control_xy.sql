654fe030979c47bbc80b228098aadeb4dc79d05d
CAMI <camicalcagno12@gmail.com>
2015-05-21 23:20:01 -0300

    triger falla

--- SQL/trigger_control_xy.sql
+++ SQL/trigger_control_xy.sql
@@ -10,3 +10,17 @@ BEGIN
 		INSERT INTO Celda values(new.id_celda,new.fila,new.columna);
 END;
 $$ delimiter ;
+
+
+delimiter $$
+CREATE TRIGGER trigger_control_xy
+BEFORE INSERT ON Celda
+FOR EACH ROW
+BEGIN
+	CREATE VIEW join_Partida_Tiene AS SELECT Partida.codigo_partida,fila,columna,orden FROM Partida,Tiene where NEW.fila=Tiene.fila and NEW.columna=Tiene.columna;
+	CREATE VIEW join_celda AS SELECT * FROM Celda NATURAL JOIN join_Partida_Tiene;
+	IF (NEW.fila>=0) and (NEW.fila<=join_Partida_Tiene.dimension_y_tablero) AND 
+		(NEW.columna>=0) and (NEW.columna<=join_Partida_Tiene.dimension_x_tablero) THEN
+		INSERT INTO Celda values (new.fila,new.columna);
+END;
+$$ delimiter ;
