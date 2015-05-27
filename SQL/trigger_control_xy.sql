853c82cbfc69b78dc806dcddf7b89d27dfc7cad3
Santiago Passalacqua <passalacquasantiago@gmail.com>
2015-05-27 00:25:25 -0300

    termine el triggergit statusgit statusgit status

--- SQL/trigger_control_xy.sql
+++ SQL/trigger_control_xy.sql
@@ -1,14 +1,16 @@
+drop trigger trigger_control_xy;
 delimiter $$
 CREATE TRIGGER trigger_control_xy
-BEFORE INSERT ON Tiene
+AFTER INSERT ON Tiene
 FOR EACH ROW
 BEGIN
 	DECLARE filas integer; -- Cantidad de filas max que tiene la partida
 	DECLARE columnas integer; -- Cantidad de columnas max que tiena la partida
     SELECT maxFilas INTO filas FROM Partida WHERE codigo_partida = NEW.codigo_partida;
     SELECT maxColumnas INTO columnas FROM Partida WHERE codigo_partida = NEW.codigo_partida;
-	IF ( NEW.fila>0) AND ( NEW.fila<=filas) AND (NEW.columna>0) AND (NEW.columna<=columnas) THEN
-    	INSERT INTO Tiene (codigo_partida,orden,fila,columna) VALUES (NEW.codigo_partida,NEW.orden,NEW.fila,NEW.columna);
+	IF ( NEW.fila<=0) OR ( NEW.fila>filas) OR (NEW.columna<=0) OR (NEW.columna>columnas) THEN
+    		 SIGNAL SQLSTATE '45000'
+			SET MESSAGE_TEXT = 'DATOS DE FILAS Y/O COLUMNAS INCORRECTOS';
     END IF;
 END;
 $$ delimiter ;
